9e9063a11fd50df15a052ae7ab0df30e0f313099
yogesh-hackx <yogesh.hackx@gmail.com>
2020-07-11 14:54:26 +0530

    :wrench: Add cloudinary config file

---
+++ src/config/cloudinaryConfig.js
@@ -0,0 +1,15 @@
+const { config, uploader } = require('cloudinary').v2;
+const dotenv = require('dotenv');
+
+dotenv.config();
+const cloudinaryConfig = (req, res, next) => {
+    config({
+        folder: 'test',
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+    next();
+};
+
+module.exports = { cloudinaryConfig, uploader };
